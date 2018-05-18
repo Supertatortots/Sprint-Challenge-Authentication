b7fa57e24c9ee836f6c159b207832de72c37e854
totsuya <supertatortots@gmail.com>
2018-05-18 12:23:57 -0400

    changed bcypt hasinga bit

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -25,13 +25,14 @@ UserSchema.pre('save', function(next) {
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
-  bcrypt.hash(this.password, 11, (err, hash) => {
-    if (err) {
-      return next (err);
-    }
+bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+  if (err) {
+    return next(err)
+  } else {
     this.password = hash;
-    return next();
-  });
+    return next()
+  }
+})
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
@@ -39,10 +40,15 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
-  bcrypt.compare(plainTextPw, this.password, (err, isMatch) => {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
+  bcrypt.compare(this.password, plainTextPW, function (err, next) {
+    if (err) {
+      return next(err)
+    } else {
+      return next()
+    }
+  }
+  )
 };
+
 
 module.exports = mongoose.model('User', UserSchema);
